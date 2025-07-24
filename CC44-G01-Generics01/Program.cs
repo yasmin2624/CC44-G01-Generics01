90f9f03902c50dc7086ec48e572878a3f46d8ba1
yasmin hossam <yasminhossam4817@gmail.com>
2025-07-24 03:56:34 +0300

    Add project files.

---
+++ CC44-G01-Generics01/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace CC44_G01_Generics01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q2-create a generic Range<T>
+
+            Range<int> intRange = new Range<int>(10, 20);
+            Console.WriteLine(intRange.IsInRange(15));  // True
+            Console.WriteLine(intRange.IsInRange(25));  // False
+            Console.WriteLine(intRange.Length());       // 10
+
+            Range<double> doubleRange = new Range<double>(5.5, 10.5);
+            Console.WriteLine(doubleRange.Length());    // 5.0
+
+
+            #endregion
+        }
+    }
+}
---
+++ CC44-G01-Generics01/Range.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CC44_G01_Generics01
+{
+    internal class Range<T> where T : IComparable<T>
+    {
+        public T Min { get; private set; }
+        public T Max { get; private set; }
+
+        public Range(T min, T max)
+        {
+            if (min.CompareTo(max) > 0)
+                throw new ArgumentException("Min must be less than or equal to Max");
+
+            Min = min;
+            Max = max;
+        }
+
+        public bool IsInRange(T value)
+        {
+            return value.CompareTo(Min) >= 0 && value.CompareTo(Max) <= 0;
+        }
+
+        public double Length()
+        {
+            return Convert.ToDouble(Max) - Convert.ToDouble(Min);
+        }
+
+    }
+}
