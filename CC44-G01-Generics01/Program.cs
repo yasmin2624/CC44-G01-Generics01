542bcaa6d5e0b8dad02f0747921fdbb086f822e1
yasmin hossam <146736348+yasmin2624@users.noreply.github.com>
2025-07-30 19:19:42 +0300

    UpdateQ1- Program.cs

--- CC44-G01-Generics01/Program.cs
+++ CC44-G01-Generics01/Program.cs
@@ -2,17 +2,67 @@
 {
     internal class Program
     {
+
+         static void BubbleSort(int[] arr)
+ {
+     int n = arr.Length;
+     bool swapped;
+
+     for (int i = 0; i < n - 1; i++)
+     {
+         swapped = false;
+
+         for (int j = 0; j < n - i - 1; j++)
+         {
+             if (arr[j] > arr[j + 1])
+             {
+                 // Swap
+                 int temp = arr[j];
+                 arr[j] = arr[j + 1];
+                 arr[j + 1] = temp;
+
+                 swapped = true;
+             }
+         }
+
+         if (!swapped)
+             break;
+     }
+ }
         static void Main(string[] args)
         {
+
+            #region Q1- Bubble Sort algorithm
+int[] arr = { 5, 1, 4, 2, 8 };
+
+Console.WriteLine("Original array:");
+foreach (int item in arr)
+{
+    Console.Write(item + " ");
+}
+Console.WriteLine();
+
+BubbleSort(arr);
+Console.WriteLine("-------------------------");
+
+Console.WriteLine("Sorted array:");
+foreach (int item in arr)
+{
+    Console.Write(item + " ");
+}
+Console.WriteLine();
+
+#endregion
+            
             #region Q2-create a generic Range<T>
 
-            Range<int> intRange = new Range<int>(10, 20);
-            Console.WriteLine(intRange.IsInRange(15));  // True
-            Console.WriteLine(intRange.IsInRange(25));  // False
-            Console.WriteLine(intRange.Length());       // 10
+            // Range<int> intRange = new Range<int>(10, 20);
+            // Console.WriteLine(intRange.IsInRange(15));  // True
+            // Console.WriteLine(intRange.IsInRange(25));  // False
+            // Console.WriteLine(intRange.Length());       // 10
 
-            Range<double> doubleRange = new Range<double>(5.5, 10.5);
-            Console.WriteLine(doubleRange.Length());    // 5.0
+            // Range<double> doubleRange = new Range<double>(5.5, 10.5);
+            // Console.WriteLine(doubleRange.Length());    // 5.0
 
 
             #endregion
